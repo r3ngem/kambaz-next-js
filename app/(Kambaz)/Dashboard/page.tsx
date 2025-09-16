a7d85c6c85816dede68d61b870fdd0428c630b78
r3ngem <paclob.m@northeastern.edu>
2025-09-16 17:49:40 -0400

    deploy errors fix

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function Dashboard() {
       <div id="wd-dashboard-courses">
         <div className="wd-dashboard-course">
           <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/reactjs.jpg" width={200} height={150} />
+            <Image src="/images/reactjs.jpg" width={200} height={150} alt="1234"/>
             <div>
               <h5> CS1234 React JS </h5>
               <p className="wd-dashboard-course-title">
@@ -20,7 +20,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course"> 
           <Link href="/Courses/3000" className="wd-dashboard-course-link">
-            <Image src="/images/shell.png" width={200} height={150} />
+            <Image src="/images/shell.png" width={200} height={150} alt="3000"/>
             <div>
               <h5> CS 3000 Algorithms and Data </h5>
               <p className="wd-dashboard-course-title">
@@ -32,7 +32,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course"> 
           <Link href="/Courses/3001" className="wd-dashboard-course-link">
-            <Image src="/images/Cinnamoroll.png" width={200} height={150} />
+            <Image src="/images/Cinnamoroll.png" width={200} height={150} alt="3001"/>
             <div>
               <h5> CS 3001 Rescitation for CS 3000 </h5>
               <p className="wd-dashboard-course-title">
@@ -44,7 +44,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course"> 
           <Link href="/Courses/4550" className="wd-dashboard-course-link">
-            <Image src="/images/chiikawa.png" width={200} height={150} />
+            <Image src="/images/chiikawa.png" width={200} height={150} alt="4550"/>
             <div>
               <h5> CS 4550 Web Development </h5>
               <p className="wd-dashboard-course-title">
@@ -56,7 +56,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course"> 
           <Link href="/Courses/2262" className="wd-dashboard-course-link">
-            <Image src="/images/miffy.png" width={200} height={150} />
+            <Image src="/images/miffy.png" width={200} height={150} alt="2262"/>
             <div>
               <h5> ARTG 2262 Prototyping with Code </h5>
               <p className="wd-dashboard-course-title">
@@ -68,7 +68,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course"> 
           <Link href="/Courses/2400" className="wd-dashboard-course-link">
-            <Image src="/images/piupiu.png" width={200} height={150} />
+            <Image src="/images/piupiu.png" width={200} height={150} alt="2400"/>
             <div>
               <h5> ARTG 2400 Interaction Design Principles </h5>
               <p className="wd-dashboard-course-title">
@@ -80,7 +80,7 @@ export default function Dashboard() {
         </div>
         <div className="wd-dashboard-course"> 
           <Link href="/Courses/2100" className="wd-dashboard-course-link">
-            <Image src="/images/snoopy.png" width={200} height={150} />
+            <Image src="/images/snoopy.png" width={200} height={150} alt="2100"/>
             <div>
               <h5> CS 2100 Program Design and Implementation 1 </h5>
               <p className="wd-dashboard-course-title">
