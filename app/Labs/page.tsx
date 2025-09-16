be84593572fcd3803626875193a05476267e0b9b
r3ngem <paclob.m@northeastern.edu>
2025-09-16 17:53:27 -0400

    add info

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -3,6 +3,8 @@ export default function Labs() {
  return (
    <div id="wd-labs">
      <h1>Labs</h1>
+     <h3>Meghan Paclob</h3>
+     <h4>CS4550.11597.202610</h4>
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
