de95122e9410082048ac551dff9828d2f6f9f345
r3ngem <paclob.m@northeastern.edu>
2025-09-16 17:13:09 -0400

    fix deploy error

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
 export default async function CoursesLayout(
-  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ id: string }> }>) {
+  { children, params }:
+    Readonly<{ children: ReactNode;
+    params: Promise<{ cid: string }> }>) {
  const { cid } = await params;
  return (
    <div id="wd-courses">
-     <h2>Courses {cid}</h2>
-     <hr />
+     <h2>Courses {cid}</h2><hr />
      <table>
        <tbody>
          <tr>
@@ -17,3 +18,4 @@ export default async function CoursesLayout(
      </table>
    </div>
 );}
+
