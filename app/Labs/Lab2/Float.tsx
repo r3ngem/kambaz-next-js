27e2005587ce91695c63fb54a50c30904380222d
r3ngem <paclob.m@northeastern.edu>
2025-09-26 08:06:44 -0400

    css examples

---
+++ app/Labs/Lab2/Float.tsx
@@ -0,0 +1,50 @@
+export default function Float() {
+    return (
+        <div id="wd-float-divs">
+            <h2>Float</h2>
+            <div>
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at suscipit ante. 
+                Maecenas fringilla leo mi, sit amet efficitur arcu varius eget. Sed in eleifend 
+                turpis. Nunc et ante at nulla porttitor porta eu quis augue. Integer vitae ante at 
+                purus ultricies blandit. Suspendisse tempus sodales posuere. Aenean pretium mollis 
+                tellus, vel posuere libero. Suspendisse eu rhoncus turpis, vel aliquet sem.
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at suscipit ante. 
+                Maecenas fringilla leo mi, sit amet efficitur arcu varius eget. Sed in eleifend 
+                turpis. Nunc et ante at nulla porttitor porta eu quis augue. Integer vitae ante at 
+                purus ultricies blandit. Suspendisse tempus sodales posuere. Aenean pretium mollis 
+                tellus, vel posuere libero. Suspendisse eu rhoncus turpis, vel aliquet sem.
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at suscipit ante. 
+                Maecenas fringilla leo mi, sit amet efficitur arcu varius eget. Sed in eleifend 
+                turpis. Nunc et ante at nulla porttitor porta eu quis augue. Integer vitae ante at
+                purus ultricies blandit. Suspendisse tempus sodales posuere. Aenean pretium mollis 
+                tellus, vel posuere libero. Suspendisse eu rhoncus turpis, vel aliquet sem.
+                <img className="wd-float-left"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at suscipit ante. 
+                Maecenas fringilla leo mi, sit amet efficitur arcu varius eget. Sed in eleifend 
+                turpis. Nunc et ante at nulla porttitor porta eu quis augue. Integer vitae ante at 
+                purus ultricies blandit. Suspendisse tempus sodales posuere. Aenean pretium mollis 
+                tellus, vel posuere libero. Suspendisse eu rhoncus turpis, vel aliquet sem.
+                <div className="wd-float-done"></div>
+            </div>
+            <h2>Float</h2>
+            <div>
+                <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+                    Yellow </div>
+                <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+                    Blue </div>
+                <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+                    Red </div>
+                <img className="wd-float-right"
+                    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+                <div className="wd-float-done"></div>
+            </div>
+        </div>
+    )
+}
