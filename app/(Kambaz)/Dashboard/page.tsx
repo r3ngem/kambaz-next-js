ed32086ad474a9362023c8228732ba0b1cfc11e9
r3ngem <paclob.m@northeastern.edu>
2025-11-03 15:02:34 -0500

    fix vercel error

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -34,7 +34,7 @@ export default function Dashboard() {
 
       <FormControl value={course.name} className="mb-2"
         onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
-      <FormControl value={course.description} rows={3}
+      <FormControl value={course.description} as="textarea"
         onChange={(e) => setCourse({ ...course, description: e.target.value }) } />
 
       <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
