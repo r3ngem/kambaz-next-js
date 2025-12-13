9233278573b640fcb125c7124841d60c6906d16b
r3ngem <paclob.m@northeastern.edu>
2025-12-12 21:50:53 -0500

    modules fix

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -38,7 +38,7 @@ export default function Modules() {
    const onCreateModuleForCourse = async () => {
     if (!cid) return;
     const newModule = { name: moduleName, course: cid };
-    const module = await client.createModuleForCourse(newModule);
+    const module = await client.createModuleForCourse(cid, newModule);
     dispatch(setModules([...modules, module]));
   };
 
